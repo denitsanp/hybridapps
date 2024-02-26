29e4989128c8541c70653a050dec4bed537fb936
denitsanp <denitsa_petkova@ue-varna.bg>
2024-02-26 22:29:41 +0200

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,1 +1,15 @@
 
+// zad. 1
+function sum(arr){
+    let num1 = Number(arr[0]);
+    let num2 = Number(arr[1]);
+    let num3 = Number(arr[2]);
+
+    let sum = num1 + num2 + num3;
+    console.log(sum);
+}
+sum(['2', '3', '4']);
+
+// zad. 2
+
+
